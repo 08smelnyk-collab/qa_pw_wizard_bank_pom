3615bed09719242b73aac35468c64a50ca6b8889
Sofiia <08.s.melnyk@gmail.com>
2026-04-07 14:23:49 +0200

    fix: address mentor feedback, remove MinStack and fix syntax errors

--- src/pages/BankHomePage.js
+++ src/pages/BankHomePage.js
@@ -1,7 +1,25 @@
-// Локатор для кнопки Consent
-const consentButton = page.getByRole('button', { name: 'Consent' });
+class BankHomePage {
+  constructor(page) {
+    // 1. Присвоюємо переданий об'єкт page властивості класу
+    this.page = page;
+    
+    // 2. Оголошуємо локатори (краще робити це в конструкторі)
+    this.consentButton = page.getByRole('button', { name: 'Consent' });
+    this.customerLoginBtn = page.locator('button[ng-click="customer()"]');
+    this.bankManagerLoginBtn = page.locator('button[ng-click="manager()"]');
+  }
 
-// Перевіряємо, чи банер видимий, і натискаємо
-if (await consentButton.isVisible()) {
-    await consentButton.click();
+  // Метод для обробки банера згоду
+  async handleConsent() {
+    // 3. Звертаємося до локатора через this
+    if (await this.consentButton.isVisible()) {
+        await this.consentButton.click();
+    }
+  }
+
+  async clickCustomerLogin() {
+    await this.customerLoginBtn.click();
+  }
 }
+
+module.exports = { BankHomePage };
